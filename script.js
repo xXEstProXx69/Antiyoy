631ad0c75f11d335ab334005408146fcb7210da5
xXEstProXx69 <xxestproxx@gmail.com>
2026-04-30 10:50:22 +0300

    improvements to end turn and unit display

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ async function GameData() {
   if (data.phase === "lobby") {
     document.getElementById("lobby").style.display = "block";
     document.getElementById("game").style.display = "none";
+    document.getElementById("end-turn").style.display = "none";
+    document.getElementById("abortbtn").style.display = "none";
+    document.getElementById("turn-text").style.display = "none";
+    document.getElementById("Your-name").style.display = "none";
   } else {
     document.getElementById("lobby").style.display = "none";
     document.getElementById("game").style.display = "block";
+    document.getElementById("end-turn").style.display = "block";
+    document.getElementById("abortbtn").style.display = "block";
+    document.getElementById("turn-text").style.display = "block";
+    document.getElementById("Your-name").style.display = "block";
   }
 
   gameState = data;
@@ -25,12 +33,18 @@ async function GameData() {
 
   container.innerHTML = "";
 
+  selectedFrom = null;
+  selectedTo = null;
+  selectedFromEl = null;
+selectedToEl = null;
+
   for (const hex of GameMap) {
     if (hex.type === "impassable") continue;
 
     const img = document.createElement("img");
     img.src = "https://tinkr.tech" + hex.image;
 
+    
     img.style.position = "absolute";
     img.style.left = hex.x + "px";
     img.style.top = hex.y + "px";
@@ -71,6 +85,20 @@ async function GameData() {
     });
 
     container.appendChild(img);
+    if (hex.unit_image) {
+  const unit = document.createElement("img");
+  unit.src = "https://tinkr.tech" + hex.unit_image;
+
+  unit.style.position = "absolute";
+  unit.style.left = hex.x + "px";
+  unit.style.top = hex.y + "px";
+  unit.style.width = hex.width + "px";
+  unit.style.height = hex.height + "px";
+
+  unit.style.pointerEvents = "none"; // IMPORTANT
+
+  container.appendChild(unit);
+}
     console.log("updating...");
   }
 }
@@ -110,11 +138,34 @@ async function AddPlayer() {
 // END TURN
 const end_turn_btn = document.getElementById("end-turn");
 
-end_turn_btn.addEventListener("click", () => {
-  if (gameState.current_player !== localStorage.getItem("username")) {
-    console.log("Not your turn!");
-    return;
-  }});
+end_turn_btn.addEventListener("click",async () => {
+  try {
+    if (!gameState) return;
+
+    if (gameState.current_player !== localStorage.getItem("username")) {
+      console.log("Not your turn!");
+      return;
+    }
+
+    const response = await fetch(DB, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        action: "end_turn",
+        player_key: localStorage.getItem("player_key")
+      })
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+  } catch (err) {
+    console.error("Error:", err);
+  }
+});
+
 
   // ADD PLAYER
 const joinbtn = document.querySelector(".btn-primary");
